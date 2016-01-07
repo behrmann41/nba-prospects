1686ba474ba7f1e4abbd0cdf3c1666c7cfb0c49d
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-07 14:18:25 -0700

    configured factory with deployed urls

--- public/javascripts/factories.js
+++ public/javascripts/factories.js
@@ -1,15 +1,15 @@
 app.factory('players', function players($http){
   var obj = {};
   obj.getYears = function () {
-    return $http.get('http://localhost:4000/players/years' || 'https://basketballradar.herokuapp.com')
+    return $http.get('http://localhost:4000/players/years' || 'https://basketballradarapi.herokuapp.com/players/years')
   }
 
   obj.getAllPlayers = function () {
-    return $http.get('http://localhost:4000/players' || 'https://basketballradar.herokuapp.com')
+    return $http.get('http://localhost:4000/players' || 'https://basketballradarapi.herokuapp.com/players')
   }
 
   obj.getColumns = function () {
-    return $http.get('http://localhost:4000/players/columns' || 'https://basketballradar.herokuapp.com')
+    return $http.get('http://localhost:4000/players/columns' || 'https://basketballradarapi.herokuapp.com/players/columns')
   }
   return obj
 })
@@ -17,7 +17,7 @@ app.factory('players', function players($http){
 app.factory('compare', function compare($http) {
   var obj = {}
   obj.getOnePlayer = function (id) {
-    return $http.get('http://localhost:4000/players/player/' + id ||'https://basketballradar.herokuapp.com/' + id)
+    return $http.get('http://localhost:4000/players/player/' + id ||'https://basketballradarapi.herokuapp.com/players/player/' + id)
   }
   return obj
 })
