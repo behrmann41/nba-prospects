947dd4523d8121036115fd3485f974431dcd8d73
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-06 19:29:32 -0700

    player percentages showing in d3 chart successfully

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -41,62 +41,16 @@ app.controller("ComparisonController", ["$scope", "compare", "$routeParams", "$r
     //Legend titles
     var LegendOptions = [player.data.name];
 
-    
+
     var stats = [];
     for (attr in player.data){
-      if (player.data[attr] !== 0 && player.data[attr] !== '0' && attr !== 'id' && attr !== 'name' && attr !== 'updatedAt' && attr !== 'createdAt' && attr !== 'draftPos' && attr !== 'draftYear'){
+      // player.data[attr] !== 0 && player.data[attr] !== '0' && attr !== 'id' && attr !== 'name' && attr !== 'updatedAt' && attr !== 'createdAt' && attr !== 'draftPos' && attr !== 'draftYear'
+      if (attr.includes('percentage') && player.data[attr] != 0){
         stats.push({axis: attr, value: player.data[attr]})
       }
     }
     //Data
     var d = [stats]
-        // [
-          // {axis:"Email",value:0.59},
-	        // {axis:"Social Networks",value:0.56},
-          // {axis:"Internet Banking",value:0.42},
-          // {axis:"News Sportsites",value:0.34},
-          // {axis:"Search Engine",value:0.48},
-          // {axis:"View Shopping sites",value:0.14},
-          // {axis:"Paying Online",value:0.11},
-          // {axis:"Buy Online",value:0.05},
-          // {axis:"Stream Music",value:0.07},
-          // {axis:"Online Gaming",value:0.12},
-          // {axis:"Navigation",value:0.27},
-          // {axis:"App connected to TV program",value:0.03},
-          // {axis:"Offline Gaming",value:0.12},
-          // {axis:"Photo Video",value:0.4},
-          // {axis:"Reading",value:0.03},
-          // {axis:"Listen Music",value:0.22},
-          // {axis:"Watch TV",value:0.03},
-          // {axis:"TV Movies Streaming",value:0.03},
-          // {axis:"Listen Radio",value:0.07},
-          // {axis:"Sending Money",value:0.18},
-          // {axis:"Other",value:0.07},
-          // {axis:"Use less Once week",value:0.08}
-          // ],[
-          // {axis:"Email",value:0.48},
-          // {axis:"Social Networks",value:0.41},
-          // {axis:"Internet Banking",value:0.27},
-          // {axis:"News Sportsites",value:0.28},
-          // {axis:"Search Engine",value:0.46},
-          // {axis:"View Shopping sites",value:0.29},
-          // {axis:"Paying Online",value:0.11},
-          // {axis:"Buy Online",value:0.14},
-          // {axis:"Stream Music",value:0.05},
-          // {axis:"Online Gaming",value:0.19},
-          // {axis:"Navigation",value:0.14},
-          // {axis:"App connected to TV program",value:0.06},
-          // {axis:"Offline Gaming",value:0.24},
-          // {axis:"Photo Video",value:0.17},
-          // {axis:"Reading",value:0.15},
-          // {axis:"Listen Music",value:0.12},
-          // {axis:"Watch TV",value:0.1},
-          // {axis:"TV Movies Streaming",value:0.14},
-          // {axis:"Listen Radio",value:0.06},
-          // {axis:"Sending Money",value:0.16},
-          // {axis:"Other",value:0.07},
-          // {axis:"Use less Once week",value:0.17}
-          // ]
 
     //Options for the Radar chart, other than default
     var mycfg = {
