fdf7cc5d833d1f80d5dbb44822ff9c238e4988ef
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-07 14:23:45 -0700

    env variable for db calls

--- public/javascripts/factories.js
+++ public/javascripts/factories.js
@@ -1,15 +1,15 @@
 app.factory('players', function players($http){
   var obj = {};
   obj.getYears = function () {
-    return $http.get('http://localhost:4000/players/years' || 'https://basketballradarapi.herokuapp.com/players/years')
+    return $http.get( process.env.APP_URL + 'players/years')
   }
 
   obj.getAllPlayers = function () {
-    return $http.get('http://localhost:4000/players' || 'https://basketballradarapi.herokuapp.com/players')
+    return $http.get( process.env.APP_URL + 'players')
   }
 
   obj.getColumns = function () {
-    return $http.get('http://localhost:4000/players/columns' || 'https://basketballradarapi.herokuapp.com/players/columns')
+    return $http.get( process.env.APP_URL + 'players/columns')
   }
   return obj
 })
@@ -17,7 +17,7 @@ app.factory('players', function players($http){
 app.factory('compare', function compare($http) {
   var obj = {}
   obj.getOnePlayer = function (id) {
-    return $http.get('http://localhost:4000/players/player/' + id ||'https://basketballradarapi.herokuapp.com/players/player/' + id)
+    return $http.get(process.env.APP_URL + 'players/player/' + id)
   }
   return obj
 })
