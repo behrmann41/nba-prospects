7574d5132d02ee3d5fdb9d53761124c3a5a0e49c
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-07 13:51:19 -0700

    configured https for d3 link

--- public/javascripts/factories.js
+++ public/javascripts/factories.js
@@ -1,15 +1,15 @@
 app.factory('players', function players($http){
   var obj = {};
   obj.getYears = function () {
-    return $http.get('http://localhost:4000/players/years')
+    return $http.get('http://localhost:4000/players/years' || 'https://basketballradar.herokuapp.com')
   }
 
   obj.getAllPlayers = function () {
-    return $http.get('http://localhost:4000/players')
+    return $http.get('http://localhost:4000/players' || 'https://basketballradar.herokuapp.com')
   }
 
   obj.getColumns = function () {
-    return $http.get('http://localhost:4000/players/columns')
+    return $http.get('http://localhost:4000/players/columns' || 'https://basketballradar.herokuapp.com')
   }
   return obj
 })
@@ -17,7 +17,7 @@ app.factory('players', function players($http){
 app.factory('compare', function compare($http) {
   var obj = {}
   obj.getOnePlayer = function (id) {
-    return $http.get('http://localhost:4000/players/player/' + id)
+    return $http.get('http://localhost:4000/players/player/' + id ||'https://basketballradar.herokuapp.com/' + id)
   }
   return obj
 })
