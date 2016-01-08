5bface245918b214f42c8ea46107f7943716c0ae
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-08 14:59:52 -0700

    modal added that provides search and ability to compare

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -27,7 +27,7 @@ app.controller("HomeController", ["$scope", function($scope){
   }
 }])
 
-app.controller("ComparisonController", ["$scope", "compare", "$routeParams", "$rootScope", "$http", function ($scope, compare, $routeParams, $rootScope, $http) {
+app.controller("ComparisonController", ["$scope", "compare", "$routeParams", "$rootScope", "$http", "players", function ($scope, compare, $routeParams, $rootScope, $http, players) {
 
   compare.getOnePlayer($routeParams.id).then(function (player) {
     return player
@@ -123,16 +123,139 @@ app.controller("ComparisonController", ["$scope", "compare", "$routeParams", "$r
         ;
   })
 
-  $scope.players = [{name: 'player'}];
-  $scope.addPlayer = function (){
-    var newItemNo = $scope.players.length+1;
-    $scope.players.push({name: "player" + newItemNo});
+  $scope.modalGlobalSearch = function () {
+    var term = $scope.globalSearchTerm
+    players.getAllPlayers().then(function (players) {
+      return players.data.data.filter(function (player) {
+        return player.name.toLowerCase().includes(term.toLowerCase());
+      })
+    }).then(function (players) {
+
+      $scope.modalPlayers = players;
+    })
   }
-  $scope.removePlayer = function (){
-    var lastItem = $scope.players.length-1;
-    if (!$scope.players[lastItem-1]) return;
-    else $scope.players.splice(lastItem);
+
+  $scope.addToGraph = function (newPlayer) {
+    compare.getOnePlayer($routeParams.id).then(function (player) {
+      return player
+    }).then(function (player) {
+      var players = [player]
+      compare.getOnePlayer(newPlayer.id).then(function (secondPlayer) {
+        players.push(secondPlayer)
+      }).then(function () {
+        //d3 functionality
+        var w = 500,
+    	    h = 500;
+
+        var colorscale = d3.scale.category10();
+
+        $scope.player1 = players[0].data;
+        $scope.player2 = players[1].data;
+        //Legend titles
+
+        var LegendOptions = [$scope.player1.name, $scope.player2.name];
+
+        $http.get('//api.pixplorer.co.uk/image?word=' + players[0].data.name + " basketball").then(function(res){
+          if(res.data.images[0]){
+            $scope.imageSearch = res.data.images[0].imageurl;
+          }
+        })
+
+        var stats1 = [];
+        for (attr in players[0].data){
+          // player.data[attr] !== 0 && player.data[attr] !== '0' && attr !== 'id' && attr !== 'name' && attr !== 'updatedAt' && attr !== 'createdAt' && attr !== 'draftPos' && attr !== 'draftYear'
+          if (attr.includes('percentage') && players[0].data[attr] != 0){
+            stats1.push({axis: attr, value: players[0].data[attr]})
+          }
+        }
+
+        var stats2 = [];
+        for (attr in players[1].data){
+          // player.data[attr] !== 0 && player.data[attr] !== '0' && attr !== 'id' && attr !== 'name' && attr !== 'updatedAt' && attr !== 'createdAt' && attr !== 'draftPos' && attr !== 'draftYear'
+          if (attr.includes('percentage') && players[1].data[attr] != 0){
+            stats2.push({axis: attr, value: players[1].data[attr]})
+          }
+        }
+        //Data
+        var d = [stats1, stats2]
+
+        //Options for the Radar chart, other than default
+        var mycfg = {
+          w: w,
+          h: h,
+          maxValue: 1.0,
+          levels: 5,
+          ExtraWidthX: 300
+        }
+
+        //Call function to draw the Radar chart
+        //Will expect that data is in %'s
+        RadarChart.draw("#chart", d, mycfg);
+
+        ////////////////////////////////////////////
+        /////////// Initiate legend ////////////////
+        ////////////////////////////////////////////
+
+        var svg = d3.select('#body')
+    	    .selectAll('svg')
+          .append('svg')
+          .attr("width", w+300)
+          .attr("height", h)
+
+        //Create the title for the legend
+        var text = svg.append("text")
+          .attr("class", "title")
+          .attr('transform', 'translate(90,0)')
+          .attr("x", w - 70)
+          .attr("y", 10)
+          .attr("font-size", "12px")
+          .attr("fill", "#404040")
+          .text("Players");
+
+        //Initiate Legend
+        var legend = svg.append("g")
+          .attr("class", "legend")
+          .attr("height", 100)
+          .attr("width", 200)
+          .attr('transform', 'translate(90,20)')
+          ;
+          //Create colour squares
+          legend.selectAll('rect')
+            .data(LegendOptions)
+            .enter()
+            .append("rect")
+            .attr("x", w - 65)
+            .attr("y", function(d, i){ return i * 20;})
+            .attr("width", 10)
+            .attr("height", 10)
+            .style("fill", function(d, i){ return colorscale(i);})
+            ;
+          //Create text next to squares
+          legend.selectAll('text')
+            .data(LegendOptions)
+            .enter()
+            .append("text")
+            .attr("x", w - 52)
+            .attr("y", function(d, i){ return i * 20 + 9;})
+            .attr("font-size", "11px")
+            .attr("fill", "#737373")
+            .text(function(d) { return d; })
+            ;
+        })
+    })
   }
+
+
+  // $scope.players = [{name: 'player'}];
+  // $scope.addPlayer = function (){
+  //   var newItemNo = $scope.players.length+1;
+  //   $scope.players.push({name: "player" + newItemNo});
+  // }
+  // $scope.removePlayer = function (){
+  //   var lastItem = $scope.players.length-1;
+  //   if (!$scope.players[lastItem-1]) return;
+  //   else $scope.players.splice(lastItem);
+  // }
 
 }])
 
