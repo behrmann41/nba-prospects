2619a020b32a76dfa54ae0fe18903447243d38df
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-11 13:46:56 -0700

    adjusted filter to rename properties

--- public/javascripts/filters.js
+++ public/javascripts/filters.js
@@ -1,9 +1,34 @@
 app.filter('playerstatsFilter', function () {
   return function(items) {
+
+    var attributes = {
+      draftYear: 'Draft Year',
+      heightWithOutShoes: 'Height Without Shoes',
+      heightWithShoes: 'Height',
+      weight: 'Weight',
+      wingspan: 'Wingspan',
+      reach: 'Reach',
+      bodyFat: 'Body Fat',
+      handLength: 'Hand Length',
+      handWidth: 'Hand Width',
+      noStepVert: 'No Step Vertical',
+      noStepVertReach: 'No Step Vertical Reach',
+      maxVert: 'Max Vertical',
+      maxVertReach: 'Max Vertical Reach',
+      bench: 'Benchpress',
+      agility: 'Agility',
+      sprint: 'Sprint',
+      rank: 'Rank'
+    }
+
     var result = {};
     angular.forEach(items, function(value, key) {
       if (!key.includes('percentage') && !key.includes('inches') && key !== 'createdAt' && key !== 'updatedAt' && key !== 'name' && key !== 'id' && value != 0) {
-        result[key] = value;
+        for (var name in attributes){
+          if (name === key){
+            result[attributes[name]] = value;
+          }
+        }
       }
     });
     return result;
